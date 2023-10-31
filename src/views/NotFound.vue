61898daf3a42f80d38225a3b17070727458543d1
Nhu-quyn <nhuquynhnguyen328@com>
2023-11-01 00:33:51 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Không thể tìm thấy trang.Trở về
+            <router-link to="/">trang chủ</router-link>
+        </p>
+
+    </div>
+</template>
