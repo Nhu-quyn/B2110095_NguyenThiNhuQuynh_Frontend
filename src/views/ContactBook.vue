657af58d032021b20f19f3fb963ab14e9e7c3caa
Nhu-quyn <nhuquynhnguyen328@com>
2023-11-01 01:46:55 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -35,15 +35,15 @@
                 </h4>
                 <ContactCard :contact="activeContact"/>
                 <router-link
-                :to="{
+                    :to="{
                     name: 'contact.edit',
                     params: { id: activeContact._id },
-                }"
+                    }"
                 >
-                <span class="mt-2 badge badge-warning">
-                    <i class="fas fa-edit"></i>Hiệu chỉnh
-                </span>
-            </router-link>
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i>Hiệu chỉnh
+                    </span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -103,9 +103,9 @@ export default {
         async retrieveContacts() {
             try {
                 this.contacts = await ContactService.getAll();
-        } catch (error) {
+            } catch (error) {
             console.log(error);
-        }
+            }
         },
         refreshList() {
             this.retrieveContacts();
